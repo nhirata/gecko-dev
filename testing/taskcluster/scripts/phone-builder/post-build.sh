200993ca2c96316a7ef57c97fa100ad99e0da17e
nhirata <nhirata.bugzilla@gmail.com>
2016-01-07 16:56:14 -0800

    Bug 1224774 - Need Nexus 5 L / Nexus 4 kk Full FOTA builds for MC

--- testing/taskcluster/scripts/phone-builder/post-build.sh
+++ testing/taskcluster/scripts/phone-builder/post-build.sh
@@ -12,10 +12,10 @@ mkdir -p $HOME/artifacts-public
 
 DEVICE=${TARGET%%-*}
 
-mv $WORKSPACE/B2G/upload/sources.xml $HOME/artifacts/sources.xml
+mv $WORKSPACE/B2G/upload/sources.xml $HOME/artifacts-public/sources.xml
 mv $WORKSPACE/B2G/upload/b2g-*.android-arm.tar.gz $HOME/artifacts/b2g-android-arm.tar.gz
 mv $WORKSPACE/B2G/upload/${TARGET}.zip $HOME/artifacts/${TARGET}.zip
-mv $WORKSPACE/B2G/upload/gaia.zip $HOME/artifacts/gaia.zip
+mv $WORKSPACE/B2G/upload/gaia.zip $HOME/artifacts-public/gaia.zip
 
 # Upload public images as public artifacts on Nexus 4 KK and Nexus 5 L
 if [ "${TARGET}" = "nexus-4-kk" -o "${TARGET}" = "nexus-5-l" ]; then
@@ -32,7 +32,11 @@ fi
 
 # FOTA full and fullimg might contain blobs
 if [ -f $WORKSPACE/B2G/upload/fota-*-update-*.mar ]; then
-  mv $WORKSPACE/B2G/upload/fota-*-update-*.mar $HOME/artifacts/
+  if [ "${TARGET}" = "nexus-4-kk" -o "${TARGET}" = "nexus-5-l" ]; then
+    mv $WORKSPACE/B2G/upload/fota-*-update-*.mar $HOME/artifacts-public/
+  else
+    mv $WORKSPACE/B2G/upload/fota-*-update-*.mar $HOME/artifacts/
+  fi
 fi
 
 # Gecko/Gaia OTA is clean
